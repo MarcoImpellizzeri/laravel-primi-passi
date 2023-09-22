9a606bf38892cb761160bffda2674b5676aad6bf
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-09-22 14:26:00 +0200

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>laravel-primi-passi</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+</head>
+
+<body>
+    {{-- 
+        - Cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
+        - Facciamo quindi sì che la rotta / visualizzi home.blade.php
+        - Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
+    --}}
+
+    
+</body>
+
+</html>
